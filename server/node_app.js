6bc6da163d67efca4550eedd9057ff35e5d13767
harsha.somarajupalli <harsha.somarajupalli@storable.com>
2023-10-12 15:44:55 +0530

    predict model changes

--- server/node_app.js
+++ server/node_app.js
@@ -2,6 +2,9 @@ var path = require("path");
 var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
+const {predict} = require('./discountGen/discountGen');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,6 +19,18 @@ app.get('/*', function(req, res, next){
   res.setHeader('Last-Modified', (new Date()).toUTCString());
   next(); 
 });
+
+app.get('/api/myroute', async (req, res)=>{
+  try {
+    const result = await predict(); // Call your async controller function here
+    res.status(200).json(result); // Send the response
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred.' });
+  }
+});
+
+
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/build/index.html"));
